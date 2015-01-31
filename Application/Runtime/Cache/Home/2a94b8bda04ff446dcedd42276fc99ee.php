7b2e38a9bd6f1bb6a8dca52b57abce5ab7f8819e
smilecc <sxcuican@163.com>
2015-01-31 14:23:24 +0800

    Start the project

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,133 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE HTML>
+<html class="no-js">
+<head>
+	  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+  <!-- Set render engine for 360 browser -->
+  <meta name="renderer" content="webkit">
+
+  <!-- No Baidu Siteapp-->
+  <meta http-equiv="Cache-Control" content="no-siteapp"/>
+
+  <link rel="icon" type="image/png" href="/Public/assets/i/favicon.png">
+
+  <!-- Add to homescreen for Chrome on Android -->
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="icon" sizes="192x192" href="/Public/assets/i/app-icon72x72@2x.png">
+
+  <!-- Add to homescreen for Safari on iOS -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="Amaze UI"/>
+  <link rel="apple-touch-icon-precomposed" href="/Public/assets/i/app-icon72x72@2x.png">
+
+  <!-- Tile icon for Win8 (144x144 + tile color) -->
+  <meta name="msapplication-TileImage" content="/Public/assets/i/app-icon72x72@2x.png">
+  <meta name="msapplication-TileColor" content="#0e90d2">
+
+  <link rel="stylesheet" href="/Public/assets/css/amazeui.min.css">
+  <link rel="stylesheet" href="/Public/assets/css/app.css">
+</head>
+<body>
+	<!-- 头部 -->
+	
+
+<header class="am-topbar">
+<div class="am-container">
+  <h1 class="am-topbar-brand">
+    <a href="#">萌娘问答</a>
+  </h1>
+
+  <div class="am-collapse am-topbar-collapse" id="doc-topbar-collapse">
+    <ul class="am-nav am-nav-pills am-topbar-nav">
+      <li class="am-active"><a href="#">首页</a></li>
+      <li><a href="#">项目</a></li>
+      <li class="am-dropdown" data-am-dropdown>
+        <a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:;">
+          下拉 <span class="am-icon-caret-down"></span>
+        </a>
+        <ul class="am-dropdown-content">
+          <li class="am-dropdown-header">标题</li>
+          <li><a href="#">1. 去月球</a></li>
+          <li class="am-active"><a href="#">2. 去火星</a></li>
+          <li><a href="#">3. 还是回地球</a></li>
+          <li class="am-disabled"><a href="#">4. 下地狱</a></li>
+          <li class="am-divider"></li>
+          <li><a href="#">5. 桥头一回首</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <form class="am-topbar-form am-topbar-left am-form-inline" role="search">
+      <div class="am-form-group">
+        <input type="text" class="am-form-field am-input-sm" placeholder="搜索">
+      </div>
+    </form>
+
+    <div class="am-topbar-right">
+      <div class="am-dropdown" data-am-dropdown="{boundary: '.am-topbar'}">
+        <button class="am-btn am-btn-secondary am-topbar-btn am-btn-sm am-dropdown-toggle" data-am-dropdown-toggle>其他 <span class="am-icon-caret-down"></span></button>
+        <ul class="am-dropdown-content">
+          <li><a href="#">注册</a></li>
+          <li><a href="#">随便看看</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="am-topbar-right">
+      <button class="am-btn am-btn-primary am-topbar-btn am-btn-sm">登录</button>
+    </div>
+  </div>
+  </div>
+</header>
+
+
+
+	<!-- /头部 -->
+
+	<!-- 主体 -->
+	<div class="am-container">
+	
+<link rel="stylesheet" href="/Public/css/index.css">
+<title>首页 - 萌娘问答</title>
+
+  <p>Hello world</p>
+
+	</div>
+	<!-- /主体 -->
+	
+	<!-- 底部 -->
+	
+
+<footer data-am-widget="footer" class="am-footer am-footer-default qa-footer-grey" data-am-footer="{  }">
+  <div class="am-footer-switch">
+    <span>萌娘问答</span>
+  </div>
+  <div class="am-footer-miscs ">
+    <p>你正在浏览的是
+      <a href="http://www.yunshipei.com/" title="萌娘百科" target="_blank" class="">萌娘百科</a> 的子项目 - 萌娘问答</p>
+    <p>CopyRight©2014 AllMoeGirl Inc.</p>
+  </div>
+</footer>
+
+
+<!--
+<script type="text/javascript">
+  document.getElementById("space_height").style.cssText="height:"+(document.body.scrollHeight-160)+"px";
+</script>
+-->
+	<!-- /底部 -->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<script src="/Public/assets/js/jquery.min.js"></script>
+<script src="/Public/assets/js/amazeui.min.js"></script>
+<!--<![endif]-->
+<!--[if lte IE 8 ]>
+<script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
+<![endif]-->
+</body>
+</html>
