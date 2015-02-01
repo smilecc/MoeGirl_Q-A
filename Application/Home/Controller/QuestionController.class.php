9455cde5ab2e6817e9d06360a6050634f0f85708
smilecc <sxcuican@163.com>
2015-02-01 19:41:09 +0800

    index page upload img is finish

---
+++ Application/Home/Controller/QuestionController.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+Class QuestionController extends Controller{
+	public function index(){
+		
+	}
+
+	public function put_question($title,$content,$anonymous){
+		
+	}
+
+	public function upload(){
+		$upload = new \Think\Upload();// 实例化上传类
+		$upload->maxSize   =     3145728;// 设置附件上传大小
+		$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+		$upload->rootPath  =      './Public/Uploads/'; // 设置附件上传目录
+		// 上传单个文件 
+		$info   =   $upload->uploadOne($_FILES['put-question-upload-input']);
+		if(!$info) {// 上传错误提示错误信息
+			$this->error($upload->getError());
+		}else{// 上传成功 获取上传文件信息
+			echo $info['savepath'].$info['savename'];
+		}
+	}
+}
--- Public/js/public.js
+++ Public/js/public.js
@@ -7,6 +7,7 @@
             }
   });
 }
+
 function login(){
   //$('#login-model').modal('open');
   $.ajax({
@@ -20,6 +21,56 @@ function login(){
             success:function(re){
             	$('#login-model').modal('close');
             	alert(re);
+                if(re=="Success"){
+                  location.reload();
+                }
+            }
+  });
+}
+
+function upload(){
+$('#put-question-upload').modal('close');
+$('#put-question-uploading').modal('open');
+    $.ajaxFileUpload({
+    url:"/index.php/Home/Question/upload.html",
+    secureuri:false,
+    fileElementId:'put-question-upload-input',
+    dataType:'text',
+    success: function (data, status){
+      $('#put-question-uploading').modal('close');
+      var reg = /error\">(.*)<\/p>/;
+      var result = reg.exec(data);
+      if(result){
+        alert(result[1]);
+        $('#put-question-upload').modal('open');
+      }
+      else{
+        alert("成功");
+        $("#put-qustion-content").val($("#put-qustion-content").val() + "{:" + data + "}");
+        $('#put-question-popup').modal('open');
+        $("#put-qustion-content").focus().select();
+      }
+    },
+    error: function (data, status, e){
+      alert(e);
+    },
+  });
+    
+}
+
+function login(){
+  //$('#login-model').modal('open');
+  $.ajax({
+            type:"POST",
+            url:"/index.php/User/Operation/login.html",
+            data:{
+                  user:$("#username").val(),
+                  pass:$("#password").val(),
+                  remember_me:$("#remember-me").val()
+                  },
+            success:function(re){
+              $('#login-model').modal('close');
+              alert(re);
                 if(re=="Success"){
                   location.reload();
                 }
