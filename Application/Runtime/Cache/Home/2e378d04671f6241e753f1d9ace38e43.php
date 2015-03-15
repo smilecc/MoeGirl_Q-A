421342df0907c7f14411d9d8165c38705db72e52
smilecc <sxcuican@163.com>
2015-03-16 02:37:19 +0800

    微调信息通知follow页面

--- Application/Runtime/Cache/Home/2e378d04671f6241e753f1d9ace38e43.php
+++ Application/Runtime/Cache/Home/2e378d04671f6241e753f1d9ace38e43.php
@@ -1,7 +1,7 @@
 <?php if (!defined('THINK_PATH')) exit();?>这些用户最近关注了你<hr />
-<?php if(is_array($timeline)): foreach($timeline as $i=>$user): ?><div class="am-text-right">
+<?php if(is_array($timeline)): foreach($timeline as $i=>$user): ?><div><button type="button" class="follow-btn-<?php echo $user['fromusername'];?> am-btn <?php echo $user['is_follow']?'':'am-btn-success';?> am-radius <?php echo $user['is_follow'] == 3?'am-icon-retweet':'';?>" onclick="follow_user('<?php echo cookie('username');?>','<?php echo $user['fromusername'];?>')"><?php echo $user['is_follow'] == 3?' ':''; echo $user['is_follow']?'取消关注':'关注TA';?></button>
 <a target="_blank" href="<?php echo U('/Home/People/'.$user['fromusername']);?>"><?php echo $user['fromusername'];?></a>
-<button type="button" class="follow-btn-<?php echo $user['fromusername'];?> am-btn <?php echo $user['is_follow']?'':'am-btn-success';?> am-radius <?php echo $user['is_follow'] == 3?'am-icon-retweet':'';?>" onclick="follow_user('<?php echo cookie('username');?>','<?php echo $user['fromusername'];?>')"><?php echo $user['is_follow'] == 3?' ':''; echo $user['is_follow']?'取消关注':'关注TA';?></button></div><hr /><?php endforeach; endif; ?>
+<hr /><?php endforeach; endif; ?>
 	
 <div class="am-alert am-alert-success" data-am-alert>
 	  <p class="am-text-center">没有更多的内容了</p>
