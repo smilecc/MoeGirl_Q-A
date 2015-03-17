8ab0f6717889df7dbd56fc923fe326dc9e30026b
smilecc <sxcuican@163.com>
2015-03-17 22:57:13 +0800

    修正timeline中answer的用户名链接

--- Application/Runtime/Cache/Home/28b1ef330ef0403dcf40049b46fc6a98.php
+++ Application/Runtime/Cache/Home/28b1ef330ef0403dcf40049b46fc6a98.php
@@ -10,17 +10,17 @@
 					      <!--<h3 class="am-comment-title">评论标题</h3>-->
 					      <small><div class="am-link-muted"><span class="am-fr"><time><?php echo $vo['pushtime'];?></time></span></div></small>
 					      <div class="am-comment-meta qustion-title-content">
-					      <?php if(count($vo['us_array']) == 1): ?><a target="_blank" href="<?php echo U('/Home/People/'.$vo['content']['username']);?>" class="am-comment-author"><?php echo $vo['username'];?></a>
+					      <?php if(count($vo['us_array']) == 1): ?><a target="_blank" href="<?php echo get_user_page($vo['username']);?>" class="am-comment-author"><?php echo $vo['username'];?></a>
 					      <?php else: ?>
-					      	<?php if(is_array($vo['us_array'])): $j = 0; $__LIST__ = $vo['us_array'];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$usname): $mod = ($j % 2 );++$j;?><a target="_blank" href="<?php echo U('/Home/People/'.$usname);?>" class="am-comment-author"><?php echo $usname;?></a>
-					      		<?php if($j < count($vo['us_array'])): ?>、<?php endif; ?>
-					      		<?php if(j == 3): ?>等人<?php endif; endforeach; endif; else: echo "" ;endif; endif; ?>
+					      	<?php if(is_array($vo['us_array'])): $j = 0; $__LIST__ = array_slice($vo['us_array'],0,3,true);if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$usname): $mod = ($j % 2 );++$j;?><a target="_blank" href="<?php echo get_user_page($usname);?>" class="am-comment-author"><?php echo $usname;?></a>
+		                        <?php if($j < count($vo['us_array'])): ?>、<?php endif; ?>
+		            			<?php if($j == 3): ?>等<?php echo count($vo['us_array']);?>人<?php endif; endforeach; endif; else: echo "" ;endif; endif; ?>
 					        <?php if($vo['status'] == 1): ?>提交了<?php elseif($vo['status'] == 2): ?>赞同了<?php else: ?>推送了<?php endif; ?>回答
 					      </div>
 					    </header>
 					    <div class="am-comment-bd">
 					    <a class="qustion-title-content am-text-truncate" target="_blank" href="/index.php/Home/Question/<?php echo $vo['content']['question_id'];?>/Answer/<?php echo $vo['content']['id'];?>"><h2><?php echo $vo['content']['question_title'];?></h2></a>
-					    <?php if($vo['status'] != 1): echo $vo['content']['username'];?><br /><?php endif; ?>
+					    <?php if($vo['status'] != 1): ?><a target="_blank" href="<?php echo get_user_page($vo['content']['username']);?>" class="am-comment-author"><?php echo $vo['content']['username'];?></a><br /><?php endif; ?>
 					      <?php echo sub_question_content($vo['content']['content']);?>
 					    </div>
 					    <p class="am-text-right"><a class="am-link-muted" href="javascript:;" value="<?php echo $vo['content']['id'];?>" name="123" onClick="javascript:comment_toggle(this,'answer');"><span class="am-icon-comment"> 评论列表</span></a></p>
@@ -34,7 +34,7 @@
 				</article>
 			<?php else: ?>
 				<small><div class="am-link-muted">
-				    <a href="#link-to-user"><?php echo $vo['username'];?></a>
+				    <a href="<?php echo get_user_page($vo['username']);?>"><?php echo $vo['username'];?></a>
 					<?php if($vo['status'] == 1): ?>提了一个问题<?php elseif($vo['status'] == 2): ?>关注了问题<?php else: ?>推送了问题<?php endif; ?> <span class="am-fr"><time><?php echo $vo['pushtime'];?></time></span>
 				</div></small>
 				<a target="_blank" href="/index.php/Home/Question/<?php echo $vo['content']['id'];?>"><h2><?php echo $vo['content']['title'];?></h2></a>
